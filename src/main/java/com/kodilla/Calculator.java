14b81a33ae48bfe5aa271acd9042c213663456d4
radekczdev <radekcz.dev@gmail.com>
2017-11-21 20:56:32 +0100

    method main() added

--- src/main/java/com/kodilla/Calculator.java
+++ src/main/java/com/kodilla/Calculator.java
@@ -9,4 +9,9 @@ public class Calculator {
         return a - b;
     }
 
+    public static void main(String args[]){
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.add(5,6));
+
+    }
 }
