27803c36314158aae2a98aa08b4304345670ee48
radekczdev <radekcz.dev@gmail.com>
2017-11-21 20:53:17 +0100

    calculator modified

--- src/main/java/com/kodilla/Calculator.java
+++ src/main/java/com/kodilla/Calculator.java
@@ -1,4 +1,12 @@
 package com.kodilla;
 
 public class Calculator {
+    public int add(int a, int b){
+        return a + b;
+    }
+
+    public int substract(int a, int b){
+        return a - b;
+    }
+
 }
